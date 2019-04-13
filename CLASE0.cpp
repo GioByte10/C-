fd704680407b8a096ab36cda174dd409eab0f0b6
Giovanni Bernal <gvanni.bernal10@gmail.com>
2019-04-13 15:24:20 -0600

    C++

---
+++ CLASE0.cpp
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+
+int main(){
+
+    int i;
+
+   printf("HelloWorld\n\n");
+   scanf("%d" &i);
+
+}
--- Photomistry.cpp
+++ Photomistry.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 using namespace std;
 
-                                   //To convert CHAR to STRING without losses use the STRING += CHAR[i]; method
-
   char element[100];
 
 
@@ -242,9 +240,10 @@ return true;
 
 int main(){
 
-  string equation, elements[10][2];
+  string equation, elements[10][2], advice;
   int i = 0, j = 0, k = 0, h, l = 0;
   bool cancel = false;
+  char Cequation[100];
 
   printf("Welcome to the Photomistry Project Tester In C++ Computer\n");
   printf("PPTCC\n");
@@ -286,8 +285,139 @@ int main(){
         }
 
 
-   //printf("%d\n", equations[0].size());
-   //cout << elements[0] << endl;
+   j = 0;
+   k = 0;
+   h = 0;
+   l = 0;
+
+
+   for(i = 0; 0 < elements[i][0].size(); i++){
+    l++;
+   }
+
+   advice = "we think there's a problem, ";
+   for(i = 0; 0 < elements[i][0].size(); i++){
+    if (elements[i][0] == "O" || elements[i][0] == "N" || elements[i][0] == "F" || elements[i][0] == "H" || elements[i][0] == "Br" || elements[i][0] == "Cl" || elements[i][0] == "I")
+       k++;
+
+   }
+
+   for (i = 0; 0 != elements[i][h].size(); i++){
+
+        if (j == k - 1 && j > 0){
+           advice += "and ";
+        }
+
+
+    if (elements[i][0] == "O"){
+        advice += "Oxygen (O2) ";
+        j++;
+  }else if (elements[i][0] == "H"){
+        advice += "Hydrogen (H2) ";
+        j++;
+  }else if (elements[i][0] == "N"){
+        advice += "Nitrogen (N2) ";
+        j++;
+  }else if (elements[i][0] == "F"){
+        advice += "Fluorine (F2) ";
+        j++;
+  }else if (elements[i][0] == "I"){
+        advice += "Iodine (I2) ";
+        j++;
+  }else if (elements[i][0] == "Cl"){
+        advice += "Chlorine (Cl2) ";
+        j++;
+  }else if (elements[i][0] == "Br"){
+        advice += "Bromine (Br2) ";
+        j++;
+  }
+
+   }
+
+   if (j > 1){
+    advice += "are diatomic elements";
+   }else{
+    advice += "is a diatomic element";
+   }
+if (j > 0){
+   cout << "In the left side of the equation, " << advice << endl;
+   printf("\n\n");
+   printf("Would you like to correct it? (y/n)");
+   cin  >> advice;
+
+   if (advice == "y" || advice == "Y"){
+
+
+
+   }
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+   j = 0;
+   k = 0;
+   h = 0;
+   l = 0;
+
+
+   for(i = 0; 0 < elements[i][1].size(); i++){
+    l++;
+   }
+
+   advice = "we think there's a problem, ";
+   for(i = 0; 0 < elements[i][1].size(); i++){
+    if (elements[i][1] == "O" || elements[i][1] == "N" || elements[i][1] == "F" || elements[i][1] == "H" || elements[i][1] == "Br" || elements[i][1] == "Cl" || elements[i][1] == "I")
+       k++;
+
+   }
+
+   for (i = 0; 0 != elements[i][1].size(); i++){
+
+        if (j == k - 1 && j > 0){
+           advice += "and ";
+        }
+
+
+    if (elements[i][1] == "O"){
+        advice += "Oxygen (O2) ";
+        j++;
+  }else if (elements[i][1] == "H"){
+        advice += "Hydrogen (H2) ";
+        j++;
+  }else if (elements[i][1] == "N"){
+        advice += "Nitrogen (N2) ";
+        j++;
+  }else if (elements[i][1] == "F"){
+        advice += "Fluorine (F2) ";
+        j++;
+  }else if (elements[i][1] == "I"){
+        advice += "Iodine (I2) ";
+        j++;
+  }else if (elements[i][1] == "Cl"){
+        advice += "Chlorine (Cl2) ";
+        j++;
+  }else if (elements[i][1] == "Br"){
+        advice += "Bromine (Br2) ";
+        j++;
+  }
+
+
+
+
+
+       // printf("%d      %d\n", j, k);
+
+   }
+
+   if (j > 1){
+    advice += "are diatomic elements";
+   }else{
+    advice += "is a diatomic element";
+   }
+if (j > 0)
+   cout << "In the Right side of the equation, " << advice << endl;
+
 printf("Left in the Equation:\n");
 
   for (i = 0; 0 != elements[i][0].size(); i++){
@@ -304,4 +434,3 @@ printf("Left in the Equation:\n");
   return 0;
 
 }
-
