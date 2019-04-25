bee05ad8238d471eefe8f69b1f16b91f36fbc74b
Giovanni Bernal <gvanni.bernal10@gmail.com>
2019-04-25 13:18:12 -0600

    C++

--- Test3.cpp
+++ Test3.cpp
@@ -1,40 +1,84 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 using namespace std;
 
-   bool GET (char x){
+struct VAR{
+	float i;
+};
 
-      if (x == 'a'){
-        return true;
-      }if (x == 'b'){
-        return 1;
-      }if (x == 'c'){
-        return true;
-      }if (x == 'd'){
-        return true;
-      }if (x == 'e'){
-        return true;
-      }if (x == 'f'){
-        return true;
-      }
+struct LINE{//k*x+a
+	float a, k;
+	VAR* x;
 
-      return false;
+	LINE(){}
+	LINE(int a) : a(a), k(0), x(0) {}
+	LINE(VAR& v) : a(0), k(1), x(&v) {}
+};
 
-   }
+LINE operator + (LINE A, LINE B){//assumes that A.x == 0 or B.x == 0 or A.x == B.x
+	LINE R;
+	R.a = A.a + B.a;
+	R.k = A.k + B.k;
+	if(A.x) R.x = A.x;
+	else R.x = B.x;
+	return R;
+}
+
+LINE operator - (LINE A, LINE B){//same as +
+	LINE R;
+	R.a = A.a - B.a;
+	R.k = A.k - B.k;
+	if(A.x) R.x = A.x;
+	else R.x = B.x;
+	return R;
+}
+
+LINE operator * (LINE A, LINE B){//assumes that A.x == 0 or B.x == 0
+	LINE R;
+	R.a = A.a * B.a;
+	R.k = A.k * B.a + B.k * A.a;
+	if(A.x) R.x = A.x;
+	else R.x = B.x;
+	return R;
+}
+
+LINE operator / (LINE A, LINE B){//assumes that B.x == 0
+	LINE R;
+	R.a = A.a / B.a;
+	R.k = A.k / B.a;
+	R.x = A.x;
+	return R;
+}
+
+void operator == (LINE A, LINE B){
+	LINE C = A - B;
+	C.x->i = -C.a/C.k;
+}
 
 int main(){
+    int a, b;
+    a + b = 3
+	int A = 3;
+	VAR B;
+	VAR C;
+	VAR D;
+	VAR E;
 
-  char y;
-  string t;
+	//5 == (2 + (y-7)*4)/2;
 
-  cin >> y;
+	2 * A == C + D;
+	B == C.i + D.i;
+	B.i == 2 * E;
+	B.i == 3 * D.i + E.i;
 
-  if(GET(y)){
-    printf("WORKS\n\n");
-  }
+    cout <<"A = " << A << endl;
+    cout <<"B = " << B.i << endl;
+	cout <<"C = " << C.i << endl;
+	cout <<"D = " << D.i << endl;
+    cout <<"E = " << E.i << endl;
 
-  system("PAUSE");
-  return 0;
+	//cout << z.i * 2 << endl;
 
+
+    system("PAUSE");
+	return 0;
 }
