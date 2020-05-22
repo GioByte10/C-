e77fd233304501fd3ef216187ac900a572c4d95a
GioByte10 <gvanni.bernal10@gmail.com>
2020-05-22 13:42:07 -0600

    C++

--- BatteryWarning.cpp
+++ BatteryWarning.cpp
@@ -9,13 +9,19 @@ int main() {
 	SYSTEM_POWER_STATUS status; // note not LPSYSTEM_POWER_STATUS
 
     int life;
+    int BTime;
 
     while(true){
 
         GetSystemPowerStatus(&status);
         life = status.BatteryLifePercent;
+        BTime = status.BatteryLifeTime;
 
-        cout << life << endl;
+        if(life > 90 && status.ACLineStatus == 1){
+            ShellExecute(NULL, "open", "C:\\Users\\super\\Desktop\\Giovanni\\Programacion\\Lenguajes\\C++\\MessageBox.vbs", NULL, NULL, SW_SHOWDEFAULT);
+            Sleep(60000);
+        }
+
         Sleep(60000);
 
     }
---
+++ MessageBox.vbs
@@ -0,0 +1,2 @@
+messageText = "______ The Batery is currently 90% or higher, unplug the charger ______"
+MsgBox messageText
