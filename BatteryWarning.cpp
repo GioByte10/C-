1fa0afc3dd941caf414dd18dd7c0146fc88724d7
GioByte10 <gvanni.bernal10@gmail.com>
2020-06-10 00:46:28 -0600

    C++

--- BatteryWarning.cpp
+++ BatteryWarning.cpp
@@ -9,20 +9,24 @@ int main() {
 	SYSTEM_POWER_STATUS status; // note not LPSYSTEM_POWER_STATUS
 
     int life;
-    int BTime;
+    int i = 0;
 
     while(true){
 
         GetSystemPowerStatus(&status);
         life = status.BatteryLifePercent;
-        BTime = status.BatteryLifeTime;
 
-        if(life > 90 && status.ACLineStatus == 1){
-            ShellExecute(NULL, "open", "C:\\Users\\super\\Desktop\\Giovanni\\Programacion\\Lenguajes\\C++\\MessageBox.vbs", NULL, NULL, SW_SHOWDEFAULT);
-            Sleep(60000);
+
+        if(life >= 90 && status.ACLineStatus == 1){
+            if(i < 10){
+                ShellExecute(NULL, "open", "C:\\Users\\super\\Desktop\\Giovanni\\Programacion\\Lenguajes\\C++\\MessageBox.vbs", NULL, NULL, SW_SHOWDEFAULT);
+                i++;
+                Sleep(60000);
+            }
+        }else{
+            i = 0;
         }
 
         Sleep(60000);
-
     }
 }
