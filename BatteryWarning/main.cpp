307219459e1451e31dd1fdaebefd3058cfd568b8
GioByte10 <gvanni.bernal10@gmail.com>
2022-07-06 16:52:28 -0600

    C++

--- BatteryWarning/main.cpp
+++ BatteryWarning/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <string>
 #include "Windows.h"
-
+#include "json/json.h"
+#include <fstream>
 using namespace std;
 
 int main() {
 
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
-
+    //ShowWindow(GetConsoleWindow(), SW_HIDE);
     SYSTEM_POWER_STATUS status; // note not LPSYSTEM_POWER_STATUS
+
+    Json::Value info;
+
+    std::ifstream infoFile("info.json", std::ifstream::binary);
+    infoFile >> info;
+
+    cout << info["path"];
 
     int life;
     int i = 0;
@@ -21,7 +28,7 @@ int main() {
 
         if(life >= 85 && status.ACLineStatus == 1){
             if(i < 10){
-                ShellExecute(NULL, "open", "C:\\Users\\super\\Desktop\\Giovanni\\Programacion\\Lenguajes\\C++\\MessageBox.vbs", NULL, NULL, SW_SHOWDEFAULT);
+                ShellExecute(NULL, "open", "MessageBox.vbs", NULL, NULL, SW_SHOWDEFAULT);
                 i++;
                 Sleep(60000);
             }
